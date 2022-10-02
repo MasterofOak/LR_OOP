b5bc9c0fd5de59f129b13ac5756d7003c02e3849
MasterofOak <108473238+MasterofOak@users.noreply.github.com>
2022-10-02 13:56:05 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,50 +1,138 @@
 ﻿#include<iostream>
+#include<string>
+#include"Longlong.h"
 using namespace std;
 
-//Створюємо структуру в якій будуть зберігатися потрібні нам змінні та функції
-struct multiply {
-	int firstNumber;
-	double secondNumber;
-	int result;
-	multiply Init(int x = 0, double y = 0);
-	multiply read();
-	multiply mult();
-	void display();
-};
-//Розписуємо функцію "INIT" щоб вона контролювали наші вхідні змінні
-multiply multiply::Init(int x , double y ) {
-	multiply m1;
-	m1.firstNumber = x;
-	m1.secondNumber = y;
-	return m1;
+//Розписуємо функцію Init для того щоб можно було звертатися до змінних в класі
+Longlong Longlong::Init(long  first_x , unsigned long first_y, long  second_x , unsigned long second_y ) {
+	Longlong tmp;
+	tmp.first_x = first_x;
+	tmp.first_y = first_y;
+	tmp.second_x = second_x;
+	tmp.second_y = second_y;
+	return tmp;
 }
-//Розписуємо функцію "READ" яка буде просити нас ввести наші дані
-multiply multiply::read() {
-	multiply m1;
-	cout<<"Input first number:" << endl;
-	cin >> m1.firstNumber ;
-	cout<<"Input second number" << endl;
-	cin >> m1.secondNumber;
-	return m1;
+
+//Розписуємо функцію read для введення початкових даних
+Longlong Longlong::read() {
+	Longlong tmp;
+	cout << "Input first number" << endl;
+	cout << "Input first half of the number" << endl;
+	cin >> tmp.first_x;
+	cout << "Input second half of the number" << endl;
+	cin >> tmp.first_y;
+	cout << "Input second number"<<endl;	
+	cout << "Input first half of the number" << endl;
+	cin >> tmp.second_x;
+	cout << "Input second half of the number" << endl;
+	cin >> tmp.second_y;
+	return tmp;
 }
-//Розписуємо функцію "MULT" яка поверне нам результат добутку 2-ох чисел
-multiply multiply::mult() {
-	multiply m1{};
-	result = firstNumber * secondNumber;
-	return m1;
+
+//Розаисуємо функцію display яка буде виводити усі потрібні розрахунки з інших функцій
+void Longlong::display() {
+	addition();
+	toString();
+	cout << "Result of addition " << str << endl;
+	subtraction();
+	toString();
+	cout << "Result of subtraction " << str << endl;
+	multiplication();
+	toString();
+	cout << "Result of multiplication " << str << endl;
+	division();
+	toString();
+	cout << "Result of division " << str << endl;
 }
-//Розписуємо функцію "DISPLAY" яка виведе данні які ми отримали
-void multiply::display() {
-	mult();
-	cout<<"Your first number: "<<firstNumber<<endl;
-	cout<<"Your second number: "<<secondNumber<<endl;
-	cout<<"Result of multiplication: "<<result<<endl;
+
+//Функція addition бере і додає введені нами дані
+Longlong Longlong::addition() {
+	Longlong tmp{};
+	//Перенесосимо змінні first_y та first_y для зручності
+	x = first_y;
+	y = second_y;
+	//Виконуємо пару циклів для того щоб в змінних x та y цілу частину перенести в дробову
+	while (x >= 1)
+	{
+		x = x / 10.0;
+	}
+	while (y >= 1)
+	{
+		y = y / 10.0;
+	}
+	result = (first_x + x) + (second_x + y);
+	return tmp;
 }
-//Об'єднуємо усе та сподіваємося що все працює
-int main() {
-	multiply m2;
-	m2 = m2.Init();
-	m2 = m2.read();
-	m2.display();
-	system("PAUSE");
+
+//Функція subtraction бере і віднімає введені нами дані
+Longlong Longlong::subtraction() {
+	Longlong tmp{};
+	//Перенесосимо змінні first_y та first_y для зручності
+	x = first_y;
+	y = second_y;
+	//Виконуємо пару циклів для того щоб в змінних x та y цілу частину перенести в дробову
+	while (x >= 1)
+	{
+		x = x / 10.0;
+	}
+	while (y >= 1)
+	{
+		y = y / 10.0;
+	}
+	result = (first_x + x) - (second_x + y);
+	return tmp;
+}
+
+//Функція multiplication бере і множе введені нами дані
+Longlong Longlong::multiplication() {
+	Longlong tmp{};
+	//Перенесосимо змінні first_y та first_y для зручності
+	x = first_y;
+	y = second_y;
+	//Виконуємо пару циклів для того щоб в змінних x та y цілу частину перенести в дробову
+	while (x >= 1)
+	{
+		x = x / 10.0;
+	}
+	while (y >= 1)
+	{
+		y = y / 10.0;
+	}
+	result = (first_x + x) * (second_x + y);
+	return tmp;
+}
+
+//Функція division бере і діле введені нами дані
+Longlong Longlong::division() {
+	Longlong tmp{};
+	//Перенесосимо змінні first_y та first_y для зручності
+	x = first_y;
+	y = second_y;
+	//Виконуємо пару циклів для того щоб в змінних x та y цілу частину перенести в дробову
+	while (x >= 1)
+	{
+		x = x / 10.0;
+	}
+	while (y >= 1)
+	{
+		y = y / 10.0;
+	}
+	result = (first_x + x) / (second_x + y);
+	return tmp;
+}
+
+//Функція toString бере цисло із змінної result і переводе її у текст
+Longlong Longlong::toString() {
+	Longlong tmp{};
+	str = to_string(result);
+	return tmp;
+}
+
+//Задаємо роботу програми
+int main(){
+	Longlong case1;
+	case1 = case1.Init();
+	case1 = case1.read();
+	case1.display();
+	system("pause");
 }
