d460d9f38f8d04f52e9ef81d4a3703b7ef752c4e
MasterofOak <108473238+MasterofOak@users.noreply.github.com>
2023-03-26 21:44:05 +0200

    Add files via upload

---
+++ LR№16/LR№16/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace LR_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.showSum = new System.Windows.Forms.TextBox();
+            this.showAver = new System.Windows.Forms.TextBox();
+            this.act_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.add_f = new System.Windows.Forms.TextBox();
+            this.sub_f = new System.Windows.Forms.TextBox();
+            this.mult_f = new System.Windows.Forms.TextBox();
+            this.div_f = new System.Windows.Forms.TextBox();
+            this.pow_f = new System.Windows.Forms.TextBox();
+            this.com_f = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сума:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Середнє:";
+            // 
+            // showSum
+            // 
+            this.showSum.Location = new System.Drawing.Point(155, 13);
+            this.showSum.Name = "showSum";
+            this.showSum.Size = new System.Drawing.Size(131, 26);
+            this.showSum.TabIndex = 7;
+            // 
+            // showAver
+            // 
+            this.showAver.Location = new System.Drawing.Point(155, 60);
+            this.showAver.Name = "showAver";
+            this.showAver.Size = new System.Drawing.Size(131, 26);
+            this.showAver.TabIndex = 8;
+            // 
+            // act_btn
+            // 
+            this.act_btn.Location = new System.Drawing.Point(47, 126);
+            this.act_btn.Name = "act_btn";
+            this.act_btn.Size = new System.Drawing.Size(113, 94);
+            this.act_btn.TabIndex = 9;
+            this.act_btn.Text = "Action";
+            this.act_btn.UseVisualStyleBackColor = true;
+            this.act_btn.Click += new System.EventHandler(this.act_btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Add";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(372, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Sub";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(372, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Mult";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Divide";
+            // 
+            // add_f
+            // 
+            this.add_f.Location = new System.Drawing.Point(466, 18);
+            this.add_f.Name = "add_f";
+            this.add_f.Size = new System.Drawing.Size(100, 26);
+            this.add_f.TabIndex = 14;
+            // 
+            // sub_f
+            // 
+            this.sub_f.Location = new System.Drawing.Point(466, 66);
+            this.sub_f.Name = "sub_f";
+            this.sub_f.Size = new System.Drawing.Size(100, 26);
+            this.sub_f.TabIndex = 15;
+            // 
+            // mult_f
+            // 
+            this.mult_f.Location = new System.Drawing.Point(466, 112);
+            this.mult_f.Name = "mult_f";
+            this.mult_f.Size = new System.Drawing.Size(100, 26);
+            this.mult_f.TabIndex = 16;
+            // 
+            // div_f
+            // 
+            this.div_f.Location = new System.Drawing.Point(466, 160);
+            this.div_f.Name = "div_f";
+            this.div_f.Size = new System.Drawing.Size(100, 26);
+            this.div_f.TabIndex = 17;
+            // 
+            // pow_f
+            // 
+            this.pow_f.Location = new System.Drawing.Point(466, 211);
+            this.pow_f.Name = "pow_f";
+            this.pow_f.Size = new System.Drawing.Size(100, 26);
+            this.pow_f.TabIndex = 18;
+            // 
+            // com_f
+            // 
+            this.com_f.Location = new System.Drawing.Point(466, 262);
+            this.com_f.Name = "com_f";
+            this.com_f.Size = new System.Drawing.Size(100, 26);
+            this.com_f.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(376, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Power";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Compare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.com_f);
+            this.Controls.Add(this.pow_f);
+            this.Controls.Add(this.div_f);
+            this.Controls.Add(this.mult_f);
+            this.Controls.Add(this.sub_f);
+            this.Controls.Add(this.add_f);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.act_btn);
+            this.Controls.Add(this.showAver);
+            this.Controls.Add(this.showSum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox showSum;
+        private System.Windows.Forms.TextBox showAver;
+        private System.Windows.Forms.Button act_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox add_f;
+        private System.Windows.Forms.TextBox sub_f;
+        private System.Windows.Forms.TextBox mult_f;
+        private System.Windows.Forms.TextBox div_f;
+        private System.Windows.Forms.TextBox pow_f;
+        private System.Windows.Forms.TextBox com_f;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
