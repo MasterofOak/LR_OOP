bfa7f3bb9260bf8b3e04ef3f84ed50407a6785f3
Vladyslav Omelchenko <108473238+MasterofOak@users.noreply.github.com>
2023-04-10 13:35:38 +0200

    Add files via upload

---
+++ LR№20/LR20/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LR20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.act_btn = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // act_btn
+            // 
+            this.act_btn.Location = new System.Drawing.Point(20, 114);
+            this.act_btn.Name = "act_btn";
+            this.act_btn.Size = new System.Drawing.Size(149, 53);
+            this.act_btn.TabIndex = 0;
+            this.act_btn.Text = "Action";
+            this.act_btn.UseVisualStyleBackColor = true;
+            this.act_btn.Click += new System.EventHandler(this.act_btn_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(20, 38);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(371, 26);
+            this.result.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.act_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button act_btn;
+        private System.Windows.Forms.TextBox result;
+    }
+}
+
---
+++ LR№20/LR20/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void act_btn_Click(object sender, EventArgs e)
+        {
+            string str = "Madam i'm Adam";
+            try
+            {
+
+                isPalindrom(str);
+            }
+            catch(Exception ex)
+            {
+                result.Text = ex.Message;
+            }
+        }
+        public void isPalindrom(string str)
+        {
+            string copy = "";
+            string[] specialChar = {"-",":","'",".","," };
+            foreach(string item in specialChar)
+            {
+                int index = str.IndexOf(item);
+                if (index >= 0)
+                {
+                    copy = str.Remove(index, 1);
+                }
+            }
+            char[] stringArray = copy.Replace(" ", "").ToLower().ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+            if (copy.Replace(" ", "").ToLower() == reversedStr)
+            {
+                result.Text = "String is palindrom";
+            }
+            else
+            {
+                throw new Exception("String is not palinfrom");
+            }
+        }
+    }
+}
