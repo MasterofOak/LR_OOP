fbdb7de3a7b24d5b7285d9fef73ceaf31bde7873
Vladyslav Omelchenko <108473238+MasterofOak@users.noreply.github.com>
2023-05-15 12:54:55 +0200

    Add files via upload

---
+++ LR№26/LR26/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LR26
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.fioBox = new System.Windows.Forms.TextBox();
+            this.uniBox = new System.Windows.Forms.TextBox();
+            this.courseBox = new System.Windows.Forms.TextBox();
+            this.principalBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fioBox
+            // 
+            this.fioBox.Location = new System.Drawing.Point(107, 30);
+            this.fioBox.Name = "fioBox";
+            this.fioBox.Size = new System.Drawing.Size(150, 31);
+            this.fioBox.TabIndex = 1;
+            // 
+            // uniBox
+            // 
+            this.uniBox.Location = new System.Drawing.Point(107, 82);
+            this.uniBox.Name = "uniBox";
+            this.uniBox.Size = new System.Drawing.Size(150, 31);
+            this.uniBox.TabIndex = 2;
+            // 
+            // courseBox
+            // 
+            this.courseBox.Location = new System.Drawing.Point(107, 132);
+            this.courseBox.Name = "courseBox";
+            this.courseBox.Size = new System.Drawing.Size(150, 31);
+            this.courseBox.TabIndex = 3;
+            // 
+            // principalBox
+            // 
+            this.principalBox.Location = new System.Drawing.Point(107, 177);
+            this.principalBox.Name = "principalBox";
+            this.principalBox.Size = new System.Drawing.Size(150, 31);
+            this.principalBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "University";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Course";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Principal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.principalBox);
+            this.Controls.Add(this.courseBox);
+            this.Controls.Add(this.uniBox);
+            this.Controls.Add(this.fioBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox fioBox;
+        private TextBox uniBox;
+        private TextBox courseBox;
+        private TextBox principalBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ LR№26/LR26/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+namespace LR26
+{
+    public partial class Form1 : Form
+    {
+        string fn = "D:\\Games\\c++\\LR26\\LR26\\Довідка про навчання.dotx";
+        Word.Application word = new Word.Application();
+        Word.Document doc = new Word.Document();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Object missingobj = System.Reflection.Missing.Value;
+            Object trueObj = true;
+            Object falseObj = false;
+            Object templatePathObj = fn;
+            doc = word.Documents.Add(ref templatePathObj, ref missingobj, ref missingobj, ref missingobj);
+            doc.Activate();
+            foreach( Word.FormField f in doc.FormFields)
+            {
+                switch (f.Name)
+                {
+                    case "fio":
+                        f.Range.Text = "\t" + fioBox.Text + "\t";
+                        break;
+                    case "uni":
+                        f.Range.Text = "\t" + uniBox.Text + "\t";
+                        break;
+                    case "course":
+                        f.Range.Text = courseBox.Text;
+                        break;
+                    case "principal":
+                        f.Range.Text = principalBox.Text;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            word.Visible = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            doc.Close();
+            word.Quit();
+            doc = null;
+            word = null;
+        }
+    }
+}
